51a6ff9c533dae0af6a9e35607179033f1a6db40
Vesstnik87 <128038285+Vesstnik87@users.noreply.github.com>
2023-03-31 11:17:20 +0500

    Просто поправил некоторые ошибки орфографические

--- lk/urls.py
+++ lk/urls.py
@@ -3,13 +3,12 @@ from django.urls import path, re_path
 from .views import *
 from . import views
 
-
 urlpatterns = [
 	path('base/', base, name='base'),
-    path('getTrump/', views.getTrump, name='getTrump'),
-    path('getTehnika/', views.getTehnika, name='getTehnika'),
-	path('sotrudniki/', views.PersonList.as_view(), name= 'sotrudniki'),
+	path('getTrump/', views.getTrump, name='getTrump'),
+	path('getTehnika/', views.getTehnika, name='getTehnika'),
 	path('ohrana_truda/', views.OhranaTruda.as_view(), name='ohrana_truda'),
+	path('sotrudniki/', views.PersonList.as_view(), name='sotrudniki'),
 	path('sotrudniki/filter', views.PersonalFilter.as_view(), name='filter'),
 	path('sotrudniki/<slug:slug>/', views.CardUser.as_view(), name='card_user'),
 	path('sotrudniki/<slug:slug>/update/', views.UserUpdate.as_view(), name='user_update'),
@@ -18,8 +17,7 @@ urlpatterns = [
 	path('register/', RegPers.as_view(), name='register'),
 	path('add_uch/', AddUch.as_view(), name='add_uch'),
 	path('remonti/', views.RemontUcastki.as_view(), name='remonti'),
-	path('remonti/<slug:slug>/', views.RemontDetail.as_view(), name = 'remont_detail'),
+	path('remonti/<slug:slug>/', views.RemontDetail.as_view(), name='remont_detail'),
 	path('upload_vtd', views.upload_vtd, name='upload_vtd'),
-
 
 ]
